fdad09a4c3d51320f3791f7b53c584d201fb6167
jidni911 <gidni441@gmail.com>
2025-04-06 18:35:46 +0600

    something

--- src/main/java/com/jidnivai/springdom/controllers/NoticeController.java
+++ src/main/java/com/jidnivai/springdom/controllers/NoticeController.java
@@ -1,9 +1,15 @@
 package com.jidnivai.springdom.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.jidnivai.springdom.entity.Notice;
+import com.jidnivai.springdom.security.services.UserDetailsImpl;
 import com.jidnivai.springdom.service.NoticeService;
 
 @RestController
@@ -11,4 +17,18 @@ import com.jidnivai.springdom.service.NoticeService;
 public class NoticeController {
     @Autowired
     NoticeService noticeService;
+
+    @GetMapping
+    Page<Notice> getNotices(
+        @RequestParam(required = false,defaultValue = "0") int page,
+        @RequestParam(required = false,defaultValue = "10") int size,
+        @AuthenticationPrincipal UserDetailsImpl userDetailsImpl
+    ) {
+        try {
+            return noticeService.getNotices( page, size, userDetailsImpl.getUser() );
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            return null;
+        }
+    }
 }
--- src/main/java/com/jidnivai/springdom/service/NoticeService.java
+++ src/main/java/com/jidnivai/springdom/service/NoticeService.java
@@ -1,8 +1,13 @@
 package com.jidnivai.springdom.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+import com.jidnivai.springdom.entity.Notice;
+import com.jidnivai.springdom.entity.User;
 import com.jidnivai.springdom.repository.NoticeRepository;
 
 @Service
@@ -10,4 +15,10 @@ public class NoticeService {
     
     @Autowired
     NoticeRepository noticeRepository;
+
+    public Page<Notice> getNotices(int page, int size, User user) {
+        Pageable pageable = PageRequest.of(page, size);
+        return noticeRepository.findAll(pageable);
+    }
+
 }
